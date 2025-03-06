21333f7b1429894c802b2b899417c4c9c8f60ffe
ncrown1190 <ncrown88@outlook.com>
2025-03-05 21:43:38 -0500

    end for question answer added

---
+++ GCStudyBuddyAPI/GCStudyBuddyAPI/DTOs/QaCreateDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace GCStudyBuddyAPI.DTOs
+{
+    public class QaCreateDTO
+    {
+        //public int Id { get; set; }
+
+        public string? Question { get; set; }
+
+        public string? Answer { get; set; }
+    }
+}
